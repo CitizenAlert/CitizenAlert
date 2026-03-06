c6d70a2bf0f33b57b6350325d56f5261ded892c6
nonocro <87670992+noaBr@users.noreply.github.com>
2026-03-06 15:18:19 +0100

    CHORE: add mdb migration auto

--- apps/api/src/config/database.config.ts
+++ apps/api/src/config/database.config.ts
@@ -5,6 +5,7 @@ export const databaseConfig = (): TypeOrmModuleOptions => ({
   url: process.env.DATABASE_URL,
   autoLoadEntities: true,
   synchronize: process.env.NODE_ENV === 'development',
+  migrationsRun: process.env.NODE_ENV === 'production',
   logging: process.env.NODE_ENV === 'development',
   entities: [__dirname + '/../**/*.entity.js'],
   migrations: [__dirname + '/../migrations/*.js'],
